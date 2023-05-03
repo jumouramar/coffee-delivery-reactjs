1980d3372736a31ac1f4b8ce68051fca05c6b281
Juliana Moura <juliana_moura@id.uff.br>
2023-05-03 19:03:29 -0300

    feat(home): style intro

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,43 @@
+import imgCoffee from "../../assets/imagem.png";
+
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
+
+import { CoffeeListContainer, HomeContainer, IntroContainer, Item, ItemsContainer, TitleContainer } from "./styles";
+
+export function Home() {
+    return (
+        <HomeContainer>
+            <IntroContainer>
+                <div>
+                    <TitleContainer>
+                        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+                        <h3>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</h3>
+                    </TitleContainer>
+                    <ItemsContainer>
+                        <Item>
+                            <ShoppingCart size={16} weight="fill" />
+                            <p>Compra simples e segura</p>
+                        </Item>
+                        <Item>
+                            <Package size={16} weight="fill" />
+                            <p>Embalagem mantém o café intacto</p>
+                        </Item>
+                        <Item>
+                            <Timer size={16} weight="fill" />
+                            <p>Entrega rápida e rastreada</p>
+                        </Item>
+                        <Item>
+                            <Coffee size={16} weight="fill" />
+                            <p>O café chega fresquinho até você</p>
+                        </Item>
+                    </ItemsContainer>
+                </div>
+                <img src={imgCoffee} alt="" />
+            </IntroContainer>
+
+            <CoffeeListContainer>
+                <h2>Nossos Cafés</h2>
+            </CoffeeListContainer>
+        </HomeContainer> 
+    );
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+    
+`;
+
+export const IntroContainer = styled.div`
+    display: flex;
+    gap: 1rem;
+    padding: 5rem 2rem;
+`;
+
+export const TitleContainer = styled.div`
+    h1 {
+        font: 800 3rem 'Baloo 2';
+        line-height: 4rem;
+        color: ${props => props.theme['gray-800']};
+        margin-bottom: 1rem;
+    }
+
+    h3 {
+        font-weight: 400;
+        font-size: 1.3rem;
+        line-height: 1.7rem;
+    }
+`;
+
+export const ItemsContainer = styled.div`
+    margin-top: 4rem;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem 0;
+`;
+
+export const Item = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    flex-basis: 50%;
+
+    & > :first-child {
+        color: ${props => props.theme['white-200']};;
+        background: ${props => props.theme['yellow-dark']};
+        border: 0.5rem solid ${props => props.theme['yellow-dark']};
+        border-radius: 999px;
+    }
+
+    p {
+        font-weight: 400;
+        font-size: 1rem;
+    }
+`;
+
+export const CoffeeListContainer = styled.div`
+
+`;
